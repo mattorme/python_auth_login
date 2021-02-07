91530ee25708da5f146e518c2a494b1ba57c9242
Matt <ormematthewj@gmail.com>
2021-02-08 07:54:10 +1100

    started account page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from wtforms.validators import Email
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4daf72f578adc41da0f41f9ef6d23cc2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from flaskblog import routes
